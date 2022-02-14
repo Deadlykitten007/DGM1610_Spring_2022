e80be474f56452ba543558416411c7c73aa324f8
Kea Hancock <10963375@myuvu.edu>
2022-02-13 17:01:37 -0700

    Simple Calculator

--- MyFirstGame/Assets/Scripts/Assigments/Opert/SimpleCalc.cs
+++ MyFirstGame/Assets/Scripts/Assigments/Opert/SimpleCalc.cs
@@ -9,18 +9,18 @@ public class SimpleCalc : MonoBehaviour
    public int numberA = 10;
    public int numberB = 33;
    public int numberC = 7;
+   public float numberD = 33.0f;
+   public float numberE = 7.0f;
 
     // + - * / % 
 
    // Start is called before the first frame update
     void Start()
     {
-      int total;
-
-      total = numberA + numberB;
-
-      Debug.Log(numberA +  " + " + numberB +" = "+ total);   
-  
+        Add();
+        Sub();
+        Mult();
+        Div();
     
     }
 
@@ -28,5 +28,48 @@ public class SimpleCalc : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Add()
+    {
+      int total;
+
+      total = numberA + numberB;
+
+      Debug.Log(numberA +" + "+ numberB +" = "+ total);
+    }
+
+    void Sub()
+    {
+      int total;
+
+      total = numberB - numberC;
+
+      Debug.Log(numberB +" - "+ numberC +" = "+ total);
+    }
+
+    void Mult()
+    {
+      int total;
+
+      total = numberB * numberB;
+
+      Debug.Log(numberB +" X "+ numberB +" = "+ total);
+    }
+
+     void Div()
+    {
+      int total;
+      int rem;
+      float tot;
+
+      total = numberB / numberC;
+      rem = numberB % numberC;
+      tot = numberD / numberE;
+
+      
+      Debug.Log(numberB +" / "+ numberC +" = "+ total);
+      Debug.Log("Remainer = "+ rem );
+      Debug.Log(numberD +" / "+ numberE +" = "+ tot);
     }
 }
