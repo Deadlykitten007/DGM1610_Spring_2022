45d0c7a02e1d11d9cda1b4b4b744ddb7500cbe4e
Kea Hancock <10963375@myuvu.edu>
2022-04-17 22:06:03 -0600

    Making Sprites And Moving Around

--- Retro FPS Clone/Assets/Scripts/PlayerController.cs
+++ Retro FPS Clone/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController instance;
     public float moveSpeed;
     private Rigidbody rb;
     private Vector3 moveInput;
     private Vector2 mouseInput;
     public float mouseSensitivity = 1.0f;
+    public Camera viewCam;
+    public GameObject bulletImpact;
+    public int currentAmmo;
+    private void Awake()
+    {
+        instance = this;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +25,44 @@ public class PlayerController : MonoBehaviour
        rb = GetComponent<Rigidbody>();
        // Hide and lock cursor
        Cursor.visible = false;
-       Cursor. lockstate = CursorLockMode.Locked; 
+       Cursor.lockState = CursorLockMode.Locked; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input = new Vector3(Input.GetAxis("Horizonal"), 0,Input.GetAxis("Vertical"));
+        moveInput = new Vector3(Input.GetAxis("Horizonal"), 0,Input.GetAxis("Vertical")); // Getting Inputs
         Vector3 moveH = transform.right * moveInput.x;
-        Vecotr3 moveV = transform.forward * moveInput.z;
+        Vector3 moveV = transform.forward * moveInput.z;
         rb.velocity = (moveH + moveV) * moveSpeed;
 
-        mouseInput = new Vector2(moveInput.GetAxis("Mouse X"), moveInput.GetAxis("Mouse Y")) * mouseSensitivity;
-        tranform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, tranform.rotation.eulerAngles.y) - mouseInput.z, tranform.rotation.eulerAngles.z);
+        mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z);
+        
+        
+        if(Input.GetMouseButtonDown(0))
+        { 
+            if(currentAmmo > 0)
+            {
+                Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log("I'm looking at" + hit.transform.name);
+                    Instantiate(bulletImpact, hit.point, transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("I'm not looking at anything!");
+                }
+
+                currentAmmo --;
+            }
+           
+
+            
+        }
+            
     }
 }
